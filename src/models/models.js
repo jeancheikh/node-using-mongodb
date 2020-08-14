f8018356a9fca76eaec27bbfc007850eb5d4b4eb
Jean Cheikh Ndiaye <jeancheikh@gmail.com>
2020-08-14 14:09:29 -0400

    Schema creation and first route

--- src/models/models.js
+++ src/models/models.js
@@ -1,2 +1,24 @@
 import mongoose from 'mongoose';
 
+const Schema = mongoose.Schema;
+
+export const ProductSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Enter a product name'
+  },
+  description: {
+    type: String,
+    required: 'Enter a descritpion'
+  },
+  category: {
+    type: String
+  },
+  price: {
+    type: Number
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
+});
