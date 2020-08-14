ace3e9d65bc182b3ccadc23ebea1c04742965ab9
Jean Cheikh Ndiaye <jeancheikh@gmail.com>
2020-08-14 13:24:28 -0400

    Adding mongoose and connection to Mongodb database

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 
 // mongoose Connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/productsdb_pluralsightPrj', {
+mongoose.connect('mongodb://localhost/productsdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
