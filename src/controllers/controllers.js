eeea2f8a734e5fb5946883932d12bb0517180bc4
Jean Cheikh Ndiaye <jeancheikh@gmail.com>
2020-08-14 14:53:56 -0400

    Update controllers.js

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -35,7 +35,6 @@ export const getProductWithID = (req, res) => {
 }
 
 export const updateProduct = (req, res) => {
-
   Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false}, (err, Product) => {
     if (err) {
       res.send(err);
@@ -45,7 +44,6 @@ export const updateProduct = (req, res) => {
 }
 
 export const deleteProduct = (req, res) => {
-
   Product.deleteOne({ _id: req.params.ProductID}, (err, Product) => {
     if (err) {
       res.send(err);
