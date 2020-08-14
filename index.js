a6343e0481d399ebffd8a22c716b41b91e7aecfe
Jean Cheikh Ndiaye <jeancheikh@gmail.com>
2020-08-14 13:22:21 -0400

    Adding mongoose

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
 
+// mongoose Connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb_pluralsightPrj', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
